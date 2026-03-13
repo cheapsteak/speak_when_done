2feadb88e9b3f19ef6da8c95e9890020f7520bd9
cheapsteak <743976+cheapsteak@users.noreply.github.com>
2026-03-13 15:36:58 -0400

    Use DEFAULT_VOICE in list_voices() response

--- speak_when_done/__init__.py
+++ speak_when_done/__init__.py
@@ -56,7 +56,7 @@ def list_voices() -> dict:
     return {
         "success": True,
         "builtin_voices": BUILTIN_VOICES,
-        "default_voice": "alba",
+        "default_voice": DEFAULT_VOICE,
         "custom_voice_hint": "You can also use a path to an audio file for voice cloning.",
     }
 
